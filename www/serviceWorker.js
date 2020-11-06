faeac83cd5e041f874b27115696b7ca1e683b10e
mantuonweb <mantuonweb@hotmail.com>
2020-11-06 17:28:31 +0530

    Update serviceWorker.js

--- www/serviceWorker.js
+++ www/serviceWorker.js
@@ -8,7 +8,9 @@ const assets = [
     "https://unpkg.com/material-components-web@latest/dist/material-components-web.min.js",
     "https://code.jquery.com/jquery-3.5.1.min.js",
     "https://fonts.googleapis.com/icon?family=Material+Icons",
-    "https://fonts.googleapis.com/css?family=Nunito:400,700&display=swap"
+    "https://fonts.googleapis.com/css?family=Nunito:400,700&display=swap",
+    "https://fonts.gstatic.com/s/materialicons/v67/flUhRq6tzZclQEJ-Vdg-IuiaDsNc.woff2",
+    "https://fonts.gstatic.com/s/nunito/v14/XRXV3I6Li01BKofINeaB.woff2"
 ]
 //https://developers.google.com/web/ilt/pwa/caching-files-with-service-worker
 self.addEventListener("install", installEvent => {
